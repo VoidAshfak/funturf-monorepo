7742885997de7d91f590080c30231df54d65706f
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-10-03 08:47:22 +0600

    fix: sticky navbar

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -12,13 +12,13 @@ export default async function AppLayout({ children }) {
     const session = await getServerSession(authOptions);
     return (
         <div>
-            <div className="hidden md:block">
+            <div className="hidden md:block sticky top-0 z-50">
                 <nav className={"navbar"}>
                     <Navbar session={session} />
                 </nav>
             </div>
 
-            <div className="block md:hidden navbar">
+            <div className="block md:hidden navbar sticky top-0 z-50">
                 <SmallScreenMenu session={session} />
             </div>
             {children}
