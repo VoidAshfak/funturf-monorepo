65ad961318e143c5a56b0be5f8592d66c8375bbc
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-10-05 21:13:48 +0600

    modified event page

--- src/app/(root)/events/page.jsx
+++ src/app/(root)/events/page.jsx
@@ -1,6 +1,8 @@
 import EventCard from "@/components/EventCard";
 import FilterVenueInput from "@/components/FilterVanueInput";
+import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/utils/getData";
+import Image from "next/image";
 import Link from "next/link";
 
 const AllEvents = async () => {
@@ -8,6 +10,26 @@ const AllEvents = async () => {
 
     return (
         <div className="w-[90%] mx-auto">
+
+            <div className="relative h-72 rounded-2xl mt-10">
+                <Image
+                    src="/assets/images/banner1.jpg"
+                    alt="banner"
+                    fill
+                    className="rounded-2xl"
+                />
+
+                <div className="absolute inset-0 bg-black/50 rounded-2xl"></div>
+
+                <div className="absolute inset-0 flex flex-col justify-center text-white px-10">
+                    <h1 className="text-3xl md:text-5xl font-bold uppercase">Find Your Game</h1>
+                    <Link href="/events/create" className="mt-12">
+                        <Button className="text-white bg-green-500 hover:cursor-pointer hover:bg-green-700">Create New Event</Button>
+                    </Link>
+                </div>
+            </div>
+
+
             <div className="my-10">
                 <FilterVenueInput title="Find Events" />
             </div>
