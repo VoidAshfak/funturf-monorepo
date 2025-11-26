ef51fb949eecbd76181780fd474a78f3fa728578
VoidAshfak <asfaqul.asif@gmail.com>
2025-11-26 22:36:47 +0600

    modify venue api

--- backend/src/routes/venue/venue.route.js
+++ backend/src/routes/venue/venue.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { 
     getVenues,
-    getVenueById 
+    getVenueById,
+    getVenueList
 } from "../../controllers/venue/venue.controller.js";
 import {verifyJWT} from "../../middlewares/auth/auth.middleware.js"
 
@@ -9,6 +10,7 @@ import {verifyJWT} from "../../middlewares/auth/auth.middleware.js"
 const router = Router();
 
 router.route("/").get(getVenues);
+router.route("/list").get(getVenueList);
 router.route('/:venue_id').get(getVenueById);
 
 
