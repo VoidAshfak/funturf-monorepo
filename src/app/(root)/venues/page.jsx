9773c0a7d5345bad74b30f1601e1ed49e1e6dd9e
VoidAshfak <asfaqul.asif@gmail.com>
2025-11-26 22:57:56 +0600

    FIX: map venue keys correctly

--- src/app/(root)/venues/page.jsx
+++ src/app/(root)/venues/page.jsx
@@ -28,10 +28,10 @@ export default async function AllVenues() {
             </div>
 
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-3 md:p-8'>
-                {venues?.map((venue) => (
+                {venues.data?.map((venue) => (
                     <Link
-                        key={venue._id}
-                        href={`/venues/${venue._id}`}
+                        key={venue.id}
+                        href={`/venues/${venue.id}`}
                     >
                         <VenueCard venue={venue} />
                     </Link>
--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -13,17 +13,17 @@ export function VenueCard({ className, venue }) {
     return (
         <Card className={`border-0 transition-all duration-300 will-change-transform hover:shadow-lg hover:-translate-y-2 hover:z-10 cursor-pointer ${className}`}>
             <CardHeader>
-                <CardTitle className="min-h-6 w-8/12 bg-gradient-to-r from-black via-blue-500 to-green-500 inline-block text-transparent bg-clip-text"> {venue?.name} </CardTitle>
+                <CardTitle className="min-h-6 w-10/12 bg-gradient-to-r from-black via-blue-500 to-green-500 inline-block text-transparent bg-clip-text"> {venue?.name} </CardTitle>
                 <CardDescription className="flex gap-1 items-center">
                     <MapPin className="w-4 shrink-0" />
                     <span className="line-clamp-1">
-                        {venue?.address}
+                        {venue?.address_line_1}
                     </span>
                 </CardDescription>
             </CardHeader>
             <CardContent>
                 <Image
-                    src={venue?.venueImages[0]}
+                    src={venue.images?.cover || "/assets/images/venue-v1"}
                     alt="Venue Image"
                     width={300}
                     height={300}
@@ -33,7 +33,7 @@ export function VenueCard({ className, venue }) {
             <CardFooter className="flex justify-between">
                 <div className="flex items-center gap-1">
                     <Clock className="w-4" />
-                    <p className="text-sm font-semibold"> {venue?.availability} </p>
+                    {/* <p className="text-sm font-semibold"> {venue?.availability} </p> */}
                 </div>
                 <div className="flex items-center gap-1">
                     <Star className="w-4 text-yellow-500" />
